9247331374e436e5fff8882a70c4b01683dbbef6
Leandre02 <2201877@etudiant.cegepvicto.ca>
2022-12-02 18:04:25 -0500

    Laboratoire A22

---
+++ Ennemi.cs
@@ -0,0 +1,130 @@
+﻿// Classe Ennemi
+//
+// Un ennemi
+//
+// L'ennemi a les mêmes statistiques et méthodes que le joueur avec quelques différences
+//  - pas d'habilete
+//  - pas de méthode pour donner les actions, l'ennemi ne fait qu'attaquer
+//  - un nouvel attribut dit si les attaques de l'ennemi sont magiques ou non
+//  - une méthode pour dire si les attaques sont magiques ou non, basée sur l'attribut
+//
+// Création : 2022/11/19
+// Par : Frédérik Taleb
+// Modification : 2022/11/24
+// Par : Frédérik Taleb
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboFinal_A22
+{
+    public class Ennemi
+    {
+        // attributs (public)
+        // un nom 
+        public string nom;
+        // att, matt, def, mdef, hp des entiers
+        public int att;
+        public int matt;
+        public int def;
+        public int mdef;
+        public int hp;
+        bool magique = false;
+
+        // magique un attribut qui détermine si les attaques sont magiques ou non
+
+        // Constructeur
+        public Ennemi(int att, int matt, int def, int mdef, int hp, string nom, bool magique)
+        {
+            //
+            // reçoit tous les attributs en paramètre
+            // assigne les paramètres aux attributs correspondants
+            this.nom = nom;
+            this.att = att;
+            this.matt = matt;
+            this.def = def;
+            this.mdef = mdef;
+            this.hp = hp;
+            this.magique = magique;
+
+        }
+        // estMagique
+        public bool estMagique()
+        {
+            //
+            // retourne l'attribut magique 
+            //
+            return this.magique;
+        }
+
+
+
+
+        // attaquer
+        public int attaquer()
+        {
+            //
+            // renvoie la statistique d'attaque
+            return this.att;
+
+        }
+
+        // defendre
+        //
+        // selon l'attaque, magique ou non, diminue les points de dommage du nombre de points de défense
+        // diminiue les points de vie selon les dommages finaux, les dommages finaux ne peuvent pas être sous 0
+        //
+        // @param bool magique vrai pour une attaque magique, faux sinon
+        // @param int dmg      le nombre de point de dommage avant la réduction par la défense
+        public void defendre(bool magique,int dmg)
+        {
+            // si l'attaque est magique
+            if (magique == true)
+            {
+                // les dommages finaux sont le dommage - la défense magique
+                dmg -= this.mdef;
+            }
+            else
+            {
+                // sinon
+
+                // les dommages finaux sont le dommage - la défense
+                dmg  -= - this.def;
+
+                // diminuer les points de vie du nombre de points de dommage final
+                this.hp -= dmg;
+            }
+        }
+
+        // estVivant
+        public bool estVivant()
+        {
+            bool estVivant = true;
+            //
+            // détermine s'il reste des points de vie
+            // 
+            // @return bool vrai s'il reste des points de vie, faux sinon
+            if (this.hp <= 0)
+            {
+                return false;
+            }
+           return estVivant;
+        }
+    
+
+            // enumererStats
+        public string enumererStats()
+        {
+            // 
+            // envoie un string contenant le nom et les points de vie
+            // "Nom : {0}, Hp : {1}"
+            //
+            string nom = $"Nom : {0}, Hp : {1} ";
+            // @return string le nom et les points de vie selon le format établi
+            return nom;
+        }
+    }
+}
